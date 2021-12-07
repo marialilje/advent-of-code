5615bfc3055371fe538941faae219bf598c7db56
Maria Tonnesen <marialiljedahl@gmail.com>
2021-12-07 17:41:03 +0000

    Day 4 Part 1, reading in board properly and changing number.

--- index.js
+++ index.js
@@ -6,33 +6,63 @@ const main = async () => {
     .toString()
     .trim()
     .split(/\r\n|\r|\n/)
-    .filter((line) => line.length > 0);
-  console.log(bingoData);
+    .filter((line) => line.trim().length > 0);
+  //console.log(bingoData);
 
   const bingoNumbers = bingoData[0].split(",").map((n) => parseInt(n));
 
+  const bingo = (board, currentPosition) => {};
   let boards = [];
 
-  for (let i = 1; bingoData.length - 4; i += 5) {
+  for (let i = 1; i < bingoData.length - 4; i += 5) {
     const board = [
       bingoData[i]
         .split(" ")
+        .filter((line) => line.trim().length > 0)
         .map((number) => ({ value: parseInt(number), isMarked: false })),
       bingoData[i + 1]
         .split(" ")
+        .filter((line) => line.trim().length > 0)
         .map((number) => ({ value: parseInt(number), isMarked: false })),
       bingoData[i + 2]
         .split(" ")
+        .filter((line) => line.trim().length > 0)
         .map((number) => ({ value: parseInt(number), isMarked: false })),
       bingoData[i + 3]
         .split(" ")
+        .filter((line) => line.trim().length > 0)
         .map((number) => ({ value: parseInt(number), isMarked: false })),
       bingoData[i + 4]
         .split(" ")
+        .filter((line) => line.trim().length > 0)
         .map((number) => ({ value: parseInt(number), isMarked: false })),
     ];
-    console.log(board);
+
+    boards.push(board);
   }
+  console.log(bingoNumbers);
+  console.log(boards[0]);
+
+  bingoNumbers.forEach((numberCalled) => {
+    for (let i = 0; i < boards.length; i++) {
+      for (let j = 0; j < boards[i].length; j++) {
+        for (let k = 0; k < boards[i][j].length; k++) {
+          let currentBoard = boards[i];
+          let currentRow = boards[i][j];
+          let currentNumber = boards[i][j][k];
+
+          if (currentNumber.value === numberCalled) {
+            currentNumber.isMarked = true;
+          }
+
+          if (bingo(currentBoard, currentNumber)) {
+            console.log("bingo");
+          }
+        }
+      }
+    }
+  });
+  console.log(boards[4]);
 
   // const createNewBoard = (number) => {
   //   for (let i = 0; i < 5; i++) {
